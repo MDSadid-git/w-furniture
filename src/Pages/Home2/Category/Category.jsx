6d0143113cb35f4cff03d57e6168344b2ee4365a
Sadid <sfhsadid@gmail.com>
2023-08-01 23:55:54 +0600

    Sadid Home 2 Category area done

--- src/Pages/Home2/Category/Category.jsx
+++ src/Pages/Home2/Category/Category.jsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
 import CategoryDetails from "./CategoryDetails";
-import bed1 from '../../../assets/ProductImage/bed1.jpg'
-import bed2 from '../../../assets/ProductImage/bed2.jpg'
-import bed3 from '../../../assets/ProductImage/bed3.jpg'
-import bed4 from '../../../assets/ProductImage/bed4.jpg'
-import sofa1 from '../../../assets/ProductImage/sofa1.jpg'
-import sofa2 from '../../../assets/ProductImage/sofa2.jpg'
-import sofa3 from '../../../assets/ProductImage/sofa3.jpg'
-import sofa4 from '../../../assets/ProductImage/sofa4.jpg'
-import light1 from '../../../assets/ProductImage/light1.jpg'
-import light2 from '../../../assets/ProductImage/light2.jpg'
-import light3 from '../../../assets/ProductImage/light3.jpg'
-import light4 from '../../../assets/ProductImage/light4.jpg'
-import dining1 from '../../../assets/ProductImage/dining1.jpg'
-import dining2 from '../../../assets/ProductImage/dining2.jpg'
-import dining3 from '../../../assets/ProductImage/dining3.jpg'
-import dining4 from '../../../assets/ProductImage/dining4.jpg'
+import bed1 from "../../../assets/ProductImage/bed1.jpg";
+import bed2 from "../../../assets/ProductImage/bed2.jpg";
+import bed3 from "../../../assets/ProductImage/bed3.jpg";
+import bed4 from "../../../assets/ProductImage/bed4.jpg";
+import sofa1 from "../../../assets/ProductImage/sofa1.jpg";
+import sofa2 from "../../../assets/ProductImage/sofa2.jpg";
+import sofa3 from "../../../assets/ProductImage/sofa3.jpg";
+import sofa4 from "../../../assets/ProductImage/sofa4.jpg";
+import light1 from "../../../assets/ProductImage/light1.jpg";
+import light2 from "../../../assets/ProductImage/light2.jpg";
+import light3 from "../../../assets/ProductImage/light3.jpg";
+import light4 from "../../../assets/ProductImage/light4.jpg";
+import dining1 from "../../../assets/ProductImage/dining1.jpg";
+import dining2 from "../../../assets/ProductImage/dining2.jpg";
+import dining3 from "../../../assets/ProductImage/dining3.jpg";
+import dining4 from "../../../assets/ProductImage/dining4.jpg";
 
 const Category = () => {
   const [living, setLiving] = useState(true);
@@ -50,13 +50,13 @@ const Category = () => {
   return (
     <div className="container mx-auto px-3">
       <div className="text-center max-w-screen-md mx-auto space-y-7 text-gray-800">
-        <h1 className="font-sans italic text-4xl font-semibold text-primary">
+        <h1 className="font-sans italic md:text-5xl text-3xl font-semibold text-primary">
           Our Categories
         </h1>
       </div>
-      <div className="mt-10 ">
+      <div className="mt-16 ">
         <div className="max-w-screen-lg mx-auto">
-          <ul className="flex justify-between md:text-2xl lg:text-4xl font-bold text-gray-800 ">
+          <ul className="flex justify-between md:text-lg lg:text-xl font-bold text-gray-800 mb-5">
             <li
               className={`${
                 living && "border-b-4 border-primary"
@@ -94,10 +94,10 @@ const Category = () => {
             <CategoryDetails
               type="New Arrivals"
               offer="15% Off Weero Living Rooms"
-              img1={sofa4} 
-                img2={sofa3} 
-                img3={sofa2} 
-                img4={sofa1}
+              img1={sofa4}
+              img2={sofa3}
+              img3={sofa2}
+              img4={sofa1}
             />
           )}
         </div>
@@ -107,9 +107,9 @@ const Category = () => {
               type="Top Selling"
               offer="10% Off Weero Bed Rooms"
               img1={bed4}
-                    img2={bed3}
-                    img3={bed2} 
-                    img4={bed1}
+              img2={bed3}
+              img3={bed2}
+              img4={bed1}
             />
           )}
         </div>
@@ -120,9 +120,9 @@ const Category = () => {
               offer="20% Off on Weero Dining Rooms"
               heading="Dining Room"
               img1={dining4}
-                    img2={dining3}
-                    img3={dining2} 
-                    img4={dining1}
+              img2={dining3}
+              img3={dining2}
+              img4={dining1}
             />
           )}
         </div>
@@ -132,9 +132,9 @@ const Category = () => {
               type="Top Selling"
               offer="20% Off Weero Lights"
               img1={light4}
-                    img2={light3}
-                    img3={light2} 
-                    img4={light1}
+              img2={light3}
+              img3={light2}
+              img4={light1}
             />
           )}
         </div>
