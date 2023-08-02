66a6a988b7e97f28fd1f3769e07125458b48ab5e
Sadid <sfhsadid@gmail.com>
2023-08-02 23:33:43 +0600

    Sadid AllVendors area done

--- src/Pages/Vendors/AllVendors/AllVendors.jsx
+++ src/Pages/Vendors/AllVendors/AllVendors.jsx
@@ -2,15 +2,14 @@ import { BsBoxes } from "react-icons/bs";
 import vendor from "../../../assets/Vendors/vendors.png";
 import { TbBrandAirtable, TbBrandXbox, TbSofa } from "react-icons/tb";
 import { Link } from "react-router-dom";
-import vendor1 from '../../../assets/ProductImage/vendor1.jpg'
-import vendor2 from '../../../assets/ProductImage/vendor2.jpg'
-import vendor3 from '../../../assets/ProductImage/vendor3.jpg'
-import vendor4 from '../../../assets/ProductImage/vendor4.jpg'
-import vendor5 from '../../../assets/ProductImage/vendor5.jpg'
-import vendor6 from '../../../assets/ProductImage/vendor6.jpg'
-import vendor7 from '../../../assets/ProductImage/vendor7.jpg'
-import vendor8 from '../../../assets/ProductImage/vendor8.jpg'
-import vendorbanner from '../../../assets/ProductImage/vendorbanner.jpg'
+import vendor1 from "../../../assets/ProductImage/vendor1.jpg";
+import vendor2 from "../../../assets/ProductImage/vendor2.jpg";
+import vendor3 from "../../../assets/ProductImage/vendor3.jpg";
+import vendor4 from "../../../assets/ProductImage/vendor4.jpg";
+import vendor5 from "../../../assets/ProductImage/vendor5.jpg";
+import vendor6 from "../../../assets/ProductImage/vendor6.jpg";
+import vendor8 from "../../../assets/ProductImage/vendor8.jpg";
+import vendorbanner from "../../../assets/ProductImage/vendorbanner.jpg";
 
 const AllVendors = () => {
   const vendors = [
@@ -19,80 +18,70 @@ const AllVendors = () => {
       name: "John Smith",
       type: "Two Brothers",
       brand: <TbBrandAirtable />,
-      image:
-        vendor1,
+      image: vendor1,
     },
     {
       id: 201,
       name: "Tina More",
       type: "BitBox",
       brand: <TbSofa />,
-      image:
-        vendor2,
+      image: vendor2,
     },
     {
       id: 202,
       name: "Kim bel",
       type: "SinCarpet",
       brand: <BsBoxes />,
-      image:
-       vendor3,
+      image: vendor3,
     },
     {
       id: 203,
       name: "Jimmy falcon",
       type: "ApeIn",
       brand: <TbBrandXbox />,
-      image:
-        vendor4,
+      image: vendor4,
     },
     {
       id: 204,
       name: "Jhon wick",
       type: "Luffi Cart",
       brand: <TbBrandXbox />,
-      image:
-        vendor5,
+      image: vendor5,
     },
     {
       id: 205,
       name: "Rita sin",
       type: "Renovatory",
       brand: <TbBrandXbox />,
-      image:
-        vendor6,
+      image: vendor6,
     },
     {
       id: 206,
       name: "Drzen",
       type: "Will bothers",
       brand: <TbBrandXbox />,
-      image:
-        vendor3,
+      image: vendor3,
     },
     {
       id: 207,
       name: "Nick",
       type: "tik right",
       brand: <TbBrandXbox />,
-      image:
-        vendor8,
+      image: vendor8,
     },
     {
       id: 208,
       name: "Watson",
       type: "swift type",
       brand: <TbBrandXbox />,
-      image:
-       vendor1,
+      image: vendor1,
     },
     {
       id: 209,
       name: "Marry Jane",
       type: "bucket wrap",
       brand: <TbBrandXbox />,
-      image:
-        vendor2,
+      image: vendor2,
     },
   ];
   return (
@@ -104,7 +93,7 @@ const AllVendors = () => {
           alt=""
         />
         <div className="absolute w-full md:top-32 top-20 overflow-hidden">
-          <h1 className="text-6xl font-semibold text-center vendorSlide">
+          <h1 className="md:text-6xl text-3xl font-semibold text-center vendorSlide">
             Our Vendors
           </h1>
           <div className="flex justify-center vendorIconSlide">
@@ -132,15 +121,17 @@ const AllVendors = () => {
             </div>
             <div className="flex justify-between items-center relative">
               <div className=" bottom-7 left-2">
-                <h1 className="text-5xl text-primary italic">{vendor.name}</h1>
+                <h1 className="text-3xl font-bold text-primary italic">
+                  {vendor.name}
+                </h1>
               </div>
               <div className="bg-primary inline-block rounded-full p-2 md:absolute right-[-60px] group-hover:right-5 duration-500">
-                <span className="text-4xl text-white">{vendor.brand}</span>
+                <span className="text-2xl text-white">{vendor.brand}</span>
               </div>
             </div>
             <div>
               <div>
-                <h1 className="text-xl font-semibold">
+                <h1 className="text-lg font-semibold">
                   <span className="text-primary">Shop Name:</span>{" "}
                   <span className="uppercase">{vendor.type}</span>
                 </h1>
